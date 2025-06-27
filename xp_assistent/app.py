cd6dfc7240ad4cbdc3edda040276e2fcb2b7b978
Timber Kerkvliet <timberkerkvliet@gmail.com>
2025-06-27 13:36:34 +0200

    loop with user input

--- xp_assistent/app.py
+++ xp_assistent/app.py
@@ -11,18 +11,30 @@ dspy.configure(lm=gemini)
 code_generator = dspy.Predict(RefactorSignature)
 
 PROJECT_DIR = '/Users/timberkerkvliet/PycharmProjects/fibonacci'
-TEST_FILE = 'test_fibonacci.py'
-PROD_FILE = 'fibo.py'
+TEST_FILE = 'fibonacci/test_fibonacci.py'
+PROD_FILE = 'fibonacci/fibo.py'
 
-with open(f'{PROJECT_DIR}/{TEST_FILE}') as f:
-    test_code = f.read()
+while True:
+    refactor_hint = input("Refactor hint: ")
+    with open(f'{PROJECT_DIR}/{TEST_FILE}') as f:
+        test_code = f.read()
 
-with open(f'{PROJECT_DIR}/{PROD_FILE}') as f:
-    prod_code = f.read()
+    with open(f'{PROJECT_DIR}/{PROD_FILE}') as f:
+        prod_code = f.read()
 
-res = code_generator(test_code=test_code, prod_code=prod_code, refactor_hint=...)
+    res = code_generator(test_code=test_code, prod_code=prod_code, refactor_hint=refactor_hint)
 
-with open(f'{PROJECT_DIR}/{PROD_FILE}', 'wb') as f:
-    f.write(res.python_code.encode())
+    with open(f'{PROJECT_DIR}/{PROD_FILE}', 'wb') as f:
+        f.write(res.python_code.encode())
 
-os.system(f"cd {PROJECT_DIR} && (python -m unittest {TEST_FILE} || git reset --hard)")
+    os.system(
+        f'''cd {PROJECT_DIR} && (
+            echo "Running tests..." &&
+            python -m unittest {TEST_FILE} &&
+            echo "✅ Tests passed. Committing..." &&
+            git commit -am "Passing tests"
+        ) || (
+            echo "❌ Tests failed. Reverting..." &&
+            git reset --hard
+        )'''
+    )
--- xp_assistent/signature.py
+++ xp_assistent/signature.py
@@ -3,10 +3,10 @@ import dspy
 
 class RefactorSignature(dspy.Signature):
     test_code = dspy.InputField(
-        desc="A set of unit tests"
+        desc="A set of unit tests that describe the behaviour of the production code. The refactored code should still pass these test."
     )
     prod_code = dspy.InputField(
-        desc="The production component"
+        desc="The production code that needs refactoring"
     )
     refactor_hint = dspy.InputField(
         desc="Refactor hint"
@@ -14,7 +14,7 @@ class RefactorSignature(dspy.Signature):
 
     # OutputField defines what we expect the model to generate.
     python_code = dspy.OutputField(
-        desc="The generated Python code.",
+        desc="The refactored production code",
         prefix="```python\n", # A prefix helps guide the model's output format.
         suffix="\n```"
     )
